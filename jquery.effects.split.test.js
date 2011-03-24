5979e30ec4e5785883019135db42fc84170f3a1f
Jesper Lindstroem Nielsen <Jesper@Webcomfort.dk>
2011-03-24 15:22:42 +0100

    Made whitespace better

--- jquery.effects.split.test.js
+++ jquery.effects.split.test.js
@@ -1,12 +1,11 @@
-$(document).ready(function(){
-     $("[data-effect]").bind('click',
-                               function(){
-									  console.log("ID: "+$(this).data('elementId'));
-                                   $('#'+$(this).data('elementId')).effect(
-                                       $(this).data('effect'),
-                                       {
-                                       },
-                                       1000 //Duration
-                                   );
-                               });
-});
+$( document ).ready(
+		function( ) {
+			$( "[data-effect]" ).bind(
+					'click',
+					function( ) {
+						console.log( "ID: " + $( this ).data( 'elementId' ) );
+						$( '#' + $( this ).data( 'elementId' ) ).effect(
+								$( this ).data( 'effect' ), {}, 1000 //Duration
+						);
+					} );
+		} );
